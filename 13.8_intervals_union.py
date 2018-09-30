a948e1a71c7f5bdda51bbd9937b43c9c9df7dc0a
BY <ybhust@gmail.com>
2018-09-30 12:27:07 -0700

    version 1
    First sort intervals taking use of the key in sort(). Closed end point comes first.
    Then iterate through each interval to conduct merging operation.
    Don't forget the last step to include the last interval!!!

--- 13.8_intervals_union.py
+++ 13.8_intervals_union.py
@@ -10,8 +10,20 @@ Interval = collections.namedtuple('Interval', ('left', 'right'))
 
 
 def union_of_intervals(intervals):
-    # TODO - you fill in here.
-    return []
+
+    intervals.sort(key=lambda e: (e.left.val, not e.left.is_closed))
+    result, temp_interval = [], intervals[0]
+    for interval in intervals:
+        if (interval.left.val <= temp_interval.right.val) and \
+                not (interval.left.val == temp_interval.right.val and
+                     interval.left.is_closed == temp_interval.right.is_closed == False):
+            temp_interval = Interval(min(temp_interval.left, interval.left, key=lambda e:(e.val, not e.is_closed)),
+                                     max(temp_interval.right, interval.right, key=lambda e:(e.val, e.is_closed)))
+        else:
+            result.append(temp_interval)
+            temp_interval = interval
+    result.append(temp_interval) # don't forget the last step!!!
+    return result
 
 
 @enable_executor_hook
