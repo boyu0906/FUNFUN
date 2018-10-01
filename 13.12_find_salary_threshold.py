b1907873043df078e33eeb3d5d4bce056fdb3ec4
BY <ybhust@gmail.com>
2018-10-01 00:45:05 -0700

    version 1
    Compare mean target salary with each current salary.

--- 13.12_find_salary_threshold.py
+++ 13.12_find_salary_threshold.py
@@ -2,8 +2,26 @@ from test_framework import generic_test
 
 
 def find_salary_cap(target_payroll, current_salaries):
-    # TODO - you fill in here.
-    return 0.0
+    num = len(current_salaries)
+    avg_salary = target_payroll / num
+    total_salary = sum(current_salaries)
+    if total_salary < target_payroll:
+        return -1
+    elif total_salary == target_payroll:
+        return max(current_salaries)
+    else:
+        num2 = 0
+        current_salaries.sort()
+        while current_salaries and current_salaries[0] < avg_salary:
+            for salary in current_salaries:
+                if salary < avg_salary:
+                    target_payroll -= salary
+                    num2 += 1
+                    current_salaries.remove(salary)
+                else:
+                    break
+            avg_salary = target_payroll / (num - num2)
+        return avg_salary
 
 
 if __name__ == '__main__':
--- 13.9_group_equal_entries.py
+++ 13.9_group_equal_entries.py
@@ -9,7 +9,7 @@ Person = collections.namedtuple('Person', ('age', 'name'))
 
 
 def group_by_age(people):
-    # TODO - you fill in here.
+    people.sort(key=lambda e:e.age)
     return
 
 
