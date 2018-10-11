ac44588d8a4496c8c70a1a0aba0b23642a264052
BY <ybhust@gmail.com>
2018-10-10 20:54:23 -0700

    version 1
    insertion sort, dummy head

--- 13.11_sort_list.py
+++ 13.11_sort_list.py
@@ -1,9 +1,23 @@
 from test_framework import generic_test
+from list_node import ListNode
 
 
 def stable_sort_list(L):
-    # TODO - you fill in here.
-    return None
+    # insertion sort
+    dummy_head = ListNode(0, L)
+    while L and L.next:
+        if L.data > L.next.data:
+            target = L.next
+            pre = dummy_head
+            while pre.next.data <= target.data:
+                pre = pre.next
+            temp = pre.next
+            pre.next = target
+            L.next = target.next
+            target.next = temp
+        else:
+            L = L.next
+    return dummy_head.next
 
 
 if __name__ == '__main__':
