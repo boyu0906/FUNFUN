4380e2fe1c9fc9b503b70e106428c81fb72b9526
BY <ybhust@gmail.com>
2018-10-13 16:34:46 -0700

    backtracking

--- 15.2_n_queens.py
+++ 15.2_n_queens.py
@@ -2,8 +2,19 @@ from test_framework import generic_test
 
 
 def n_queens(n):
-    # TODO - you fill in here.
-    return []
+    result, col_pos = [], [0]*n
+
+    def n_queens_helper(row):
+        if row == n:
+            result.append(list(col_pos))
+            return
+        for col in range(n):
+            if all(abs(col - c) not in (0, row - i) for i, c in enumerate(col_pos[:row])):
+                col_pos[row] = col
+                n_queens_helper(row + 1)
+
+    n_queens_helper(0)
+    return result
 
 
 def comp(a, b):
