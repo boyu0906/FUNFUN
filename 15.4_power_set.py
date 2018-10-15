88cdca7f2e957cbe035f3705674ac69df62ca646
BY <ybhust@gmail.com>
2018-10-14 23:10:42 -0700

    version 3
    EPI solution, seems slower than version 1, but faster than version 2.

--- 15.4_power_set.py
+++ 15.4_power_set.py
@@ -9,21 +9,31 @@ def generate_power_set(S):
     #     return [s + [input_set[0]] for s in sub_result] + [s for s in sub_result]
     # return helper(S)
 
-    def backtrack(a, k, A):
-        if k == len(A):
-            temp = []
-            for i in range(len(A)):
-                if a[i] == 1:
-                    temp.append(A[i])
-            result.append(temp)
-        else:
-            for c in [1, 0]:
-                a[k] = c
-                backtrack(a, k + 1, A)
-    result = []
-    a = [0] * len(S)
-    backtrack(a, 0, S)
-    return result
+    # def backtrack(a, k, A):
+    #     if k == len(A):
+    #         temp = []
+    #         for i in range(len(A)):
+    #             if a[i] == 1:
+    #                 temp.append(A[i])
+    #         result.append(temp)
+    #     else:
+    #         for c in [1, 0]:
+    #             a[k] = c
+    #             backtrack(a, k + 1, A)
+    # result = []
+    # a = [0] * len(S)
+    # backtrack(a, 0, S)
+    # return result
+
+    def helper(to_be_selected, selected_so_far):
+        if to_be_selected == len(S):
+            power_set.append(list(selected_so_far))
+            return
+        helper(to_be_selected + 1, selected_so_far)
+        helper(to_be_selected + 1, selected_so_far + [S[to_be_selected]])
+    power_set = []
+    helper(0, [])
+    return power_set
 
 if __name__ == '__main__':
     exit(
