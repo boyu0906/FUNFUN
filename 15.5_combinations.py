83b180e69fd9d214286e060126b9ff287eb3f2f5
BY <ybhust@gmail.com>
2018-10-15 22:36:01 -0700

    version 1-backtracking

--- 15.5_combinations.py
+++ 15.5_combinations.py
@@ -2,8 +2,21 @@ from test_framework import generic_test, test_utils
 
 
 def combinations(n, k):
-    # TODO - you fill in here.
-    return []
+    if k == 0:
+        return [[]]
+
+    def helper(i, sub_set, candidates):
+        if i == k:
+            result.append(list(sub_set))   # list is needed!
+            return
+        for j in range(len(candidates)):
+            sub_set[i] = candidates[j]
+            curr_candidates = candidates[j + 1:]    # update candidates for the next round
+            helper(i + 1, sub_set, curr_candidates)
+
+    result, initial_set = [], [0]*k
+    helper(0, initial_set, list(range(1, n + 1)))
+    return result
 
 
 if __name__ == '__main__':
