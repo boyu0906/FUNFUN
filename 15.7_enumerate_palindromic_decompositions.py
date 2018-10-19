58465b228401489350982f0c6ac1d1933ebda3c7
BY <ybhust@gmail.com>
2018-10-18 21:24:54 -0700

    version 1
    My initial version.

--- 15.7_enumerate_palindromic_decompositions.py
+++ 15.7_enumerate_palindromic_decompositions.py
@@ -2,8 +2,19 @@ from test_framework import generic_test
 
 
 def palindrome_decompositions(input):
-    # TODO - you fill in here.
-    return []
+    def helper(S, partial):
+        if len(''.join(partial)) == N:
+            result.append(partial)
+        for i in range(len(S)):
+            if is_palindrom(S[:i+1]):
+                helper(S[i+1:], partial + [S[:i+1]])
+
+    def is_palindrom(S):
+        return S == S[::-1]
+    result = []
+    N = len(input)
+    helper(input, [])
+    return result
 
 
 def comp(a, b):
