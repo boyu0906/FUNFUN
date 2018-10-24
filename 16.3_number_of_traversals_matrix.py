ab302c7cfebaebba55225a7b58e4379c9bfb7950
BY <ybhust@gmail.com>
2018-10-23 22:19:50 -0700

    version 1
    bottom up DP.

--- 16.3_number_of_traversals_matrix.py
+++ 16.3_number_of_traversals_matrix.py
@@ -2,8 +2,15 @@ from test_framework import generic_test
 
 
 def number_of_ways(n, m):
-    # TODO - you fill in here.
-    return 0
+    num_ways = [[0]*m for _ in range(n)]
+    for i in range(m):
+        num_ways[0][i] = 1
+    for j in range(n):
+        num_ways[j][0] = 1
+    for i in range(1, n):
+        for j in range(1, m):
+            num_ways[i][j] = num_ways[i][j - 1] + num_ways[i - 1][j]
+    return num_ways[n - 1][m - 1]
 
 
 if __name__ == '__main__':
