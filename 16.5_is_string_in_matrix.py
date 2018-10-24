b2c3a7734969c6816b7191b45f98a7d06afddab8
BY <ybhust@gmail.com>
2018-10-23 22:44:59 -0700

    version 1
    backtracking, it is possible to visit an entry in the grid more than once.

--- 16.5_is_string_in_matrix.py
+++ 16.5_is_string_in_matrix.py
@@ -2,8 +2,26 @@ from test_framework import generic_test
 
 
 def is_pattern_contained_in_grid(grid, S):
-    # TODO - you fill in here.
-    return True
+    def helper(candidate, k):
+        if k == len(S):
+            return True
+        for i, j in candidate:
+            if grid[i][j] == S[k]:
+                new_candidate = []
+                if i - 1 >= 0:
+                    new_candidate.append((i - 1, j))
+                if i + 1 < len(grid):
+                    new_candidate.append((i + 1, j))
+                if j - 1 >= 0:
+                    new_candidate.append((i, j - 1))
+                if j + 1 < len(grid[0]):
+                    new_candidate.append((i, j + 1))
+                result = helper(new_candidate, k + 1)
+                if result:
+                    return True
+        return False
+    initial_candidate = [(i, j) for i in range(len(grid)) for j in range(len(grid[0]))]
+    return helper(initial_candidate, 0)
 
 
 if __name__ == '__main__':
