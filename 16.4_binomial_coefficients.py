b2d591600907b70f31f7da3b359e2162c4704665
BY <ybhust@gmail.com>
2018-10-24 21:36:56 -0700

    version 1
    C(n, k) = (n - k + 1)/k * C(n, k - 1)

--- 16.4_binomial_coefficients.py
+++ 16.4_binomial_coefficients.py
@@ -2,8 +2,15 @@ from test_framework import generic_test
 
 
 def compute_binomial_coefficient(n, k):
-    # TODO - you fill in here.
-    return 0
+    if k == 0:
+        return 1
+    result = [1, n] + [0] * (k - 1)
+    for i in range(2, k + 1):
+        if n - i <= k and result[n - i] != 0:
+            result[i] = result[n - i]
+        else:
+            result[i] = result[i - 1] * (n - i + 1) / i
+    return result[-1]
 
 
 if __name__ == '__main__':
