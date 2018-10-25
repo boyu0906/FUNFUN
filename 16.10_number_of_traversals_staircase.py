cb32ebbd7fa591ccd887d9b7121e2e19e9541dcf
BY <ybhust@gmail.com>
2018-10-24 20:51:27 -0700

    version 1
    number of options for the last step.

--- 16.10_number_of_traversals_staircase.py
+++ 16.10_number_of_traversals_staircase.py
@@ -2,8 +2,11 @@ from test_framework import generic_test
 
 
 def number_of_ways_to_top(top, maximum_step):
-    # TODO - you fill in here.
-    return 0
+    num_ways = [1] + [0] * top
+    for i in range(1, top + 1):
+        for j in range(1, min(maximum_step,i) + 1):
+            num_ways[i] += num_ways[i - j]
+    return num_ways[-1]
 
 
 if __name__ == '__main__':
